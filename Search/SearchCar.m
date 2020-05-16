1fd894415d07b356534583c69c9d86e0b49f6b31
Alon (PC) <downtown2u@gmail.com>
2020-05-16 23:26:54 +0300

    Removed broken and unused function

--- Search/SearchCar.m
+++ Search/SearchCar.m
@@ -11,15 +11,7 @@ classdef SearchCar < Car
             % the current position of the car
             outputObj = CarSearchPosition(obj, obj.xPos, obj.yPos, obj.Rotation);
         end
-        
-        function outputObj = getCurSearchPositionHistory(obj, lastPointObj)
-            % Returns an "CarPositionHistory" object that contains
-            % the current position of the car, with a pointer to the
-            % last position (parameter).
-            outputObj = CarPositionHistory(obj.xPos, obj.yPos, obj.Rotation, lastPointObj);
-        end
-        
-        
+    
     end
 end
 
