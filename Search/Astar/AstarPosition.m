39e89fc0f6735463273fdfed223289a3baee3229
Alon (PC) <downtown2u@gmail.com>
2020-05-20 01:47:57 +0300

    Added the pulling strength value to each positon

--- Search/Astar/AstarPosition.m
+++ Search/Astar/AstarPosition.m
@@ -5,21 +5,28 @@ classdef AstarPosition < DijkstraPosition
     
     properties (SetAccess = protected, GetAccess = public)
         distanceToGoal  % The estimated cost to the goal
+        pullStrength  % Multiplier of the distance cost. default value is 1
     end
     
     methods
         
-        function obj = AstarPosition(car, xPos, yPos, Rotation, endPoint, cost)
+        function obj = AstarPosition(car, xPos, yPos, Rotation, endPoint, cost, pullingStrength)
             % Calls "DijkstraPosition" constructor
             % and sets the cost of the position to infinity.
             
             obj = obj@DijkstraPosition(car, xPos, yPos, Rotation);
             
             if (nargin > 5)
-                obj.tryUpdateCost(cost);
+                obj.tryUpdateCost(cost)
+            end
+            
+            if (nargin > 6)
+                obj.setPullingStrength(pullingStrength)
+            else
+                obj.setPullingStrength(1)
             end
 
-            obj.setEndGoal(endPoint);
+            obj.setEndGoal(endPoint)
         end
         
         function setEndGoal(obj, endPoint)
@@ -42,7 +49,24 @@ classdef AstarPosition < DijkstraPosition
         function cost = getTotalCost(obj)
             % Returns the total cost of the current position
             % (Used in the A* Algorithm!)
-            cost = obj.getCost() + obj.getDistanceToGoal();
+            cost = obj.getCost() + (obj.getDistanceToGoal() * obj.getPullingStrength());
+        end
+        
+        function strength = getPullingStrength(obj)
+            % Returns the pulling strength of the distance to the goal.
+            % If this value is high, the algorithm will give more weight to
+            % the distance from the position to the end point, and if this
+            % value is low the algorithm will give more weight to the
+            % number of steps. The default value is 1.
+            strength = obj.pullStrength;
+        end
+        
+        function setPullingStrength(obj, weight)
+            % If this value is high, the algorithm will give more weight to
+            % the distance from the position to the end point, and if this
+            % value is low the algorithm will give more weight to the
+            % number of steps. The default value is 1.
+            obj.pullStrength = weight;
         end
         
     end
