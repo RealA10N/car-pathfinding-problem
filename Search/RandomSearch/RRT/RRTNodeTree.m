490693ee705bb19f8de331a5d68981ccf37c4d3e
Alon (PC) <downtown2u@gmail.com>
2020-10-19 19:39:20 +0300

    for -> parfor (RRT search)

--- Search/RandomSearch/RRT/RRTNodeTree.m
+++ Search/RandomSearch/RRT/RRTNodeTree.m
@@ -32,7 +32,8 @@ classdef RRTNodeTree < handle
             distances = zeros(1, length(obj.tree));
             
             % Loops over each node in the tree
-            for i=1:length(obj.tree)
+            distances = inf(length(obj.tree),1);
+            parfor i=1:length(obj.tree)
                 % For each node calculate the distance between the node and
                 % the given position
                 distances(i) = obj.parent_algorithm.twoNodesDistance(positionObj, obj.tree(i));
